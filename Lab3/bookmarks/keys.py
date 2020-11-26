0fda43859e654306c50f9c8dea2bbb4efdf66168
EskaValier <d.soczek1999@gmail.com>
2020-11-26 14:55:52 +0100

    Create social-website
    Strona z działającym uwierzytelnieniem fb, twitter nie działa

---
+++ Lab3/bookmarks/keys.py
@@ -0,0 +1,14 @@
+from django import forms
+
+class KeysAndSecret(forms.Form):
+    fb_key = "209260457272046"
+    fb_secret = "0a7b5b7ec36fd1cf5eb2089b8e1aac4f"
+
+    instagram_key = "214091723419361"
+    instagram_secret = "3189a41433f83692100910480c9fb71e"
+
+    google_key = "620629174956-jgtf6pbc3921s8a3oonq0b7q3p8os211.apps.googleusercontent.com"
+    google_secret = "6Mgs66IjBfGKLm5-cwqZs-b1"
+
+    twitter_key = "IxusTUKFdIlBxcKtLfKZd0CVG"
+    twitter_secret = "9OsZAmEPQVZCx9scxJYBhKmhSaYHrPq2zLRsGhkjpiIYJMeJxv"
