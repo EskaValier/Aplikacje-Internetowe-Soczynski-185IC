40940cc58786fc9f2af9013283d6f94ce0f6ca4d
EskaValier <d.soczek1999@gmail.com>
2021-01-27 14:06:25 +0100

    Create WebWorker on other site, Counting FibNoumbers

--- Lab8/worker.js
+++ Lab8/worker.js
@@ -1,10 +1,15 @@
 
 var i = 0;
+var j = 1;
+var FibNumber = 0;
 
-function timedCount() {
-  i = i + 1;
-  postMessage(i);
-  setTimeout("timedCount()",500);
+//obliczanie kolejnych liczb ciagu fibonaciego
+function timedCountFibNumber() {
+  FibNumber = i + j;
+  i=j;
+  j=FibNumber;
+  postMessage(FibNumber);
+  setTimeout("timedCount()",1000);
 }
 
-timedCount();
+timedCountFibNumber();
